72ce0390310bccf952257fb2153904cec73e88a3
hussainAlJ <149183695+hussainALJ@users.noreply.github.com>
2025-10-03 09:17:48 +0300

    Add write function
    to write text on the screen

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
+//Selector for the Screen
+const screen = document.querySelector("#screen");
+
+//Selectors for Actions
 const clear = document.querySelector("#clear");
 const plusMinus = document.querySelector(`#pm`);
 const percent = document.querySelector("#percent");
 
+//Selectors for Operations
 const division = document.querySelector("#divide");
 const multiplication = document.querySelector("#multiply");
 const minus = document.querySelector("#minus");
 const plus = document.querySelector("#plus");
 const equal = document.querySelector("#equal");
 
+//Selectors for Numbers
 const oneBtn = document.querySelector("#one");
 const twoBtn = document.querySelector("#two");
 const threeBtn = document.querySelector("#three");
@@ -17,3 +23,7 @@ const sixBtn = document.querySelector("#six");
 const sevenBtn = document.querySelector("#seven");
 const eightBtn = document.querySelector("#eight");
 const nineBtn = document.querySelector("#nine");
+
+function write(a){ 
+    screen.textContent = `${a}`;
+}
