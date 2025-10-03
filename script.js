5bd97ebbaa36230022072417406a7ee700a3602d
hussainAlJ <149183695+hussainALJ@users.noreply.github.com>
2025-10-03 12:50:54 +0300

    Change event listener to one in the buttons div
    For better mangment

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
-//Selector for the Screen
+//Selectors
 const screen = document.querySelector("#screen");
 
-//Selectors for Actions
 const clear = document.querySelector("#clear");
 const plusMinus = document.querySelector(`#pm`);
 const percent = document.querySelector("#percent");
 
-//Selectors for Operations
 const division = document.querySelector("#divide");
 const multiplication = document.querySelector("#multiply");
 const minus = document.querySelector("#minus");
 const plus = document.querySelector("#plus");
 const equal = document.querySelector("#equal");
 
-//Selectors for Numbers
 const oneBtn = document.querySelector("#one");
 const twoBtn = document.querySelector("#two");
 const threeBtn = document.querySelector("#three");
@@ -23,71 +20,48 @@ const sixBtn = document.querySelector("#six");
 const sevenBtn = document.querySelector("#seven");
 const eightBtn = document.querySelector("#eight");
 const nineBtn = document.querySelector("#nine");
-const zeroBtn = document.querySelector("#zero")
+const zeroBtn = document.querySelector("#zero");
+const point = document.querySelector("#decimal");
 
-function write(a){ 
-    if (Number.isInteger(a)) {
-        if (operator === null) {
-            if (firstNum === null) {
-                firstNum = a;
-            }else {
-                firstNum = Number(firstNum += `${a}`)
-            }
-            num = firstNum;
-        }else {
-            if (secondNum === null) {
-                secondNum = a;
-            }else {
-                secondNum = Number(secondNum += `${a}`)
-            }
-            num = secondNum;
-        }
-        screen.textContent = num;
-    }else {
-        
+const buttons = document.querySelector(".buttons");
+buttons.addEventListener("click", (e) => {
+    let button = e.target;
+    switch (button) {
+        case zeroBtn:
+            break;
+        case oneBtn:
+            break;
+        case twoBtn:
+            break;
+        case threeBtn:
+            break;
+        case fourBtn:
+            break;
+        case fiveBtn:
+            break;
+        case sixBtn:
+            break;
+        case sevenBtn:
+            break;
+        case eightBtn:
+            break;
+        case nineBtn:
+            break;
+        case plus:
+            break;
+        case minus:
+            break;
+        case multiplication:
+            break;
+        case division:
+            break;
+        case equal:
+            break;
+        case plusMinus:
+            break;
+        case percent:
+            break;
+        case clear:
+            break;
     }
-}
-
-let firstNum = null;
-let operator = null;
-let secondNum = null;
-
-oneBtn.addEventListener("click", () => {
-    write(1);
-})
-
-twoBtn.addEventListener("click", () => {
-    write(2);
-})
-
-threeBtn.addEventListener("click", () => {
-    write(3);
-})
-
-fourBtn.addEventListener("click", () => {
-    write(4);
-})
-
-fiveBtn.addEventListener("click", () => {
-    write(5);
-})
-
-sixBtn.addEventListener("click", () => {
-    write(6);
-})
-
-sevenBtn.addEventListener("click", () => {
-    write(7);
-})
-
-eightBtn.addEventListener("click", () => {
-    write(8);
-})
-
-nineBtn.addEventListener("click", () => {
-    write(9);
-})
-
-zeroBtn.addEventListener("click", () => {
-    write(0);
 })
