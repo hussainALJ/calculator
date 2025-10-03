babd45a334b964b5e0768f7e09c76f982262ea0d
hussainAlJ <149183695+hussainALJ@users.noreply.github.com>
2025-10-03 11:00:27 +0300

    Add number buttons event listiener
    For writing the numbers

--- script.js
+++ script.js
@@ -23,7 +23,71 @@ const sixBtn = document.querySelector("#six");
 const sevenBtn = document.querySelector("#seven");
 const eightBtn = document.querySelector("#eight");
 const nineBtn = document.querySelector("#nine");
+const zeroBtn = document.querySelector("#zero")
 
 function write(a){ 
-    screen.textContent = `${a}`;
+    if (Number.isInteger(a)) {
+        if (operator === null) {
+            if (firstNum === null) {
+                firstNum = a;
+            }else {
+                firstNum = Number(firstNum += `${a}`)
+            }
+            num = firstNum;
+        }else {
+            if (secondNum === null) {
+                secondNum = a;
+            }else {
+                secondNum = Number(secondNum += `${a}`)
+            }
+            num = secondNum;
+        }
+        screen.textContent = num;
+    }else {
+        
+    }
 }
+
+let firstNum = null;
+let operator = null;
+let secondNum = null;
+
+oneBtn.addEventListener("click", () => {
+    write(1);
+})
+
+twoBtn.addEventListener("click", () => {
+    write(2);
+})
+
+threeBtn.addEventListener("click", () => {
+    write(3);
+})
+
+fourBtn.addEventListener("click", () => {
+    write(4);
+})
+
+fiveBtn.addEventListener("click", () => {
+    write(5);
+})
+
+sixBtn.addEventListener("click", () => {
+    write(6);
+})
+
+sevenBtn.addEventListener("click", () => {
+    write(7);
+})
+
+eightBtn.addEventListener("click", () => {
+    write(8);
+})
+
+nineBtn.addEventListener("click", () => {
+    write(9);
+})
+
+zeroBtn.addEventListener("click", () => {
+    write(0);
+})
