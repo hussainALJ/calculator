65fa27d85e49e9e4919fcd811986fdb870699836
hussainAlJ <149183695+hussainALJ@users.noreply.github.com>
2025-10-03 08:52:41 +0300

    Add selctors to the buttons

---
+++ script.js
@@ -0,0 +1,19 @@
+const clear = document.querySelector("#clear");
+const plusMinus = document.querySelector(`#pm`);
+const percent = document.querySelector("#percent");
+
+const division = document.querySelector("#divide");
+const multiplication = document.querySelector("#multiply");
+const minus = document.querySelector("#minus");
+const plus = document.querySelector("#plus");
+const equal = document.querySelector("#equal");
+
+const oneBtn = document.querySelector("#one");
+const twoBtn = document.querySelector("#two");
+const threeBtn = document.querySelector("#three");
+const fourBtn = document.querySelector("#four");
+const fiveBtn = document.querySelector("#five");
+const sixBtn = document.querySelector("#six");
+const sevenBtn = document.querySelector("#seven");
+const eightBtn = document.querySelector("#eight");
+const nineBtn = document.querySelector("#nine");
